7cfa0fb3e7af6afae43172a16109ac19e7a73739
YaninaShokun04 <84152248+YaninaShokun04@users.noreply.github.com>
2021-11-20 21:33:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButton button("Hello, Aleksandr Vladimirovich!");
 button.resize(200, 60);
 button.show( );
  
